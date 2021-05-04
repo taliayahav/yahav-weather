abf60cdece2da3978c57e238b2132f9fe44d942c
Talia Yahav <taliayahav@unknown.Level3.net>
2021-05-04 17:50:32 -0400

    added test file

---
+++ src/test/java/yahav/openweathermap/OpenWeatherMapControllerTest.java
@@ -0,0 +1,24 @@
+package yahav.openweathermap;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class OpenWeatherMapControllerTest {
+    @BeforeClass
+    public static void beforeClass() {
+        com.sun.javafx.application.PlatformImpl.startup(() -> {
+        });
+    }
+    @Test
+    public void initialize(){
+        //given
+    }
+    @Test
+    public void onOpenWeatherMapFeed(){
+
+    }
+    @Test
+    public void onOpenWeatherMapForecast(){
+
+    }
+}
